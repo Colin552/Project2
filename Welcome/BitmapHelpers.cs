8af54d608fc8df6a58789105a390efb8183bce8f
Colin Strong <colin-strong@hotmail.com>
2018-04-27 21:57:04 -0400

    Add project files.

---
+++ Welcome/BitmapHelpers.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * 
+ * Everything taken from https://github.com/xamarin/recipes/tree/master/Recipes/android/other_ux/camera_intent/take_a_picture_and_save_using_camera_app
+ * 
+ */
+namespace Welcome
+{
+    using System.IO;
+
+    using Android.Graphics;
+
+    public static class BitmapHelpers
+    {
+        public static Bitmap LoadAndResizeBitmap(this string fileName, int width, int height)
+        {
+            // First we get the the dimensions of the file on disk
+            BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
+            BitmapFactory.DecodeFile(fileName, options);
+
+            // Next we calculate the ratio that we need to resize the image by
+            // in order to fit the requested dimensions.
+            int outHeight = options.OutHeight;
+            int outWidth = options.OutWidth;
+            int inSampleSize = 1;
+
+            if (outHeight > height || outWidth > width)
+            {
+                inSampleSize = outWidth > outHeight
+                                   ? outHeight / height
+                                   : outWidth / width;
+            }
+
+            // Now we will load the image and have BitmapFactory resize it for us.
+            options.InSampleSize = inSampleSize;
+            options.InJustDecodeBounds = false;
+            Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
+
+            return resizedBitmap;
+        }
+    }
+}
---
+++ Welcome/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * 
+ * 
+ * 
+ */
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace Welcome
+{
+    [Activity(Label = "Tissue Emporium", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button learnButton = FindViewById<Button>(Resource.Id.btnLearn);
+            Button photoButton = FindViewById<Button>(Resource.Id.btnPhoto);
+
+            learnButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(Learn));
+                StartActivity(intent);
+            };
+
+            photoButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(Photo));
+                StartActivity(intent);
+            };
+
+
+        }
+
+        
+    }
+}
+
