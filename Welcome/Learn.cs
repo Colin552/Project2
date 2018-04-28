319f030b1cbf2887af8ba018e32b7ce985636b38
Calvin Lapp <calvin.lapp@dcmail.ca>
2018-04-28 19:26:22 -0400

    Added stuff to Learn.axml and Learn.cs for concert shows

--- Welcome/Learn.cs
+++ Welcome/Learn.cs
@@ -20,7 +20,38 @@ namespace Welcome
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Learn);
 
+            Button firstButton = FindViewById<Button>(Resource.Id.btnBuy1);
+            Button secondButton = FindViewById<Button>(Resource.Id.btnBuy2);
+            Button thirdButton = FindViewById<Button>(Resource.Id.btnBuy3);
+
+            firstButton.Click += (sender, e) =>
+            {
+                DoStuffWhenClicked();
+            };
+
+            secondButton.Click += (sender, e) =>
+            {
+                DoStuffWhenClicked();
+            };
+
+            thirdButton.Click += (sender, e) =>
+            {
+                DoStuffWhenClicked();
+            };
             // Create your application here
+        }
+
+        public void DoStuffWhenClicked()
+        {
+            Android.App.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            AlertDialog alert = dialog.Create();
+            alert.SetTitle("Sold Out");
+            alert.SetMessage("Sorry, all tickets are currently sold out.");
+            alert.SetButton("OK", (c, ev) =>
+            {
+                // Ok button click task  
+            });
+            alert.Show();
         }
     }
 }
